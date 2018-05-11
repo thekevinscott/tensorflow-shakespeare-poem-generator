bfead54f60a90de8937ac80843c7f7ce855114d7
Kevin Scott <kevin@hitheory.com>
2018-05-11 15:06:28 -0400

    Stop and remove previous images

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+docker stop text-generation
+docker rm text-generation
 docker build -t text-generation:latest .
 NV_GPU=1 nvidia-docker run -w /code -v $(pwd)/src:/app/src --name text-generation --runtime=nvidia -e NVIDIA_DRIVER_CAPABILITIES=compute,utility -e NVIDIA_VISIBLE_DEVICES=all style-transfer-playground-ai-api
 
--- start.sh
+++ start.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
+docker stop text-generation
+docker rm text-generation
 NV_GPU=1 nvidia-docker run -w /code -v $(pwd)/src:/app/src --name text-generation --runtime=nvidia -e NVIDIA_DRIVER_CAPABILITIES=compute,utility -e NVIDIA_VISIBLE_DEVICES=all style-transfer-playground-ai-api
