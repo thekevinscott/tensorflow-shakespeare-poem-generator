f825aaba0a2c9d6a8db7bc58f5421032b7c9b108
Kevin Scott <kevin@hitheory.com>
2018-05-11 14:47:14 -0400

    Pass args to script

--- start.sh
+++ start.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-NV_GPU=1 nvidia-docker run -w /code -v $(pwd)/src:/code -it tensorflow/tensorflow:latest-gpu-py3 python3 ./rnn_train.py
+NV_GPU=1 nvidia-docker run -w /code -v $(pwd)/src:/code -it
+tensorflow/tensorflow:latest-gpu-py3 python3 ./rnn_train.py
+--corpus=shakespeare --output=rnn_training_yar
