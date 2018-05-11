48bb945fe8135134c1b1a6fc83cc7eee467ced88
Kevin Scott <kevin@hitheory.com>
2018-05-11 15:08:51 -0400

    Simplify nvidia-docker

--- start.sh
+++ start.sh
@@ -2,6 +2,4 @@
 
 docker stop text-generation
 docker rm text-generation
-NV_GPU=1 nvidia-docker run -w /code -v $(pwd)/src:/app/src --name
-text-generation --runtime=nvidia -e NVIDIA_DRIVER_CAPABILITIES=compute,utility
--e NVIDIA_VISIBLE_DEVICES=all text-generation
+NV_GPU=1 nvidia-docker run --name text-generation
