e05d82045d752b9720308de5e7870b4cb6566e24
Kevin Scott <kevin@hitheory.com>
2018-05-11 14:49:47 -0400

    Collapse line

--- start.sh
+++ start.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-NV_GPU=1 nvidia-docker run -w /code -v $(pwd)/src:/code -it
-tensorflow/tensorflow:latest-gpu-py3 python3 ./rnn_train.py
+NV_GPU=1 nvidia-docker run -w /code -v $(pwd)/src:/code -it tensorflow/tensorflow:latest-gpu-py3 python3 ./rnn_train.py
