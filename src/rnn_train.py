f8a018897e41fc85b3e89517adc9f22a0fb55851
Kevin Scott <kevin@hitheory.com>
2018-05-11 14:48:40 -0400

    Hardcore vars

--- src/rnn_train.py
+++ src/rnn_train.py
@@ -35,6 +35,9 @@ parser.add_argument('--output', help='The name of the output model')
 
 args = parser.parse_args()
 
+args.corpus = "shakespeare"
+args.output = "outtttty"
+
 tf.set_random_seed(0)
 
 device_lib.list_local_devices()
